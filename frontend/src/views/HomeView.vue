fb99c6f2faabccffcde9556610d33e8e1db9cab3
Paul Watson <paulewiii3@gmail.com>
2023-11-10 19:56:14 -0500

    add prod docker

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,7 +10,7 @@
         <div class="max-w-3xl flex flex-col">
           <h1 class="text-4xl font-semibold">Welcome!</h1>
           <p class="text-xl">Dui ut ornare lectus sit amet est. Id aliquet risus feugiat in ante metus dictum. Bibendum neque egestas congue quisque egestas. Habitasse platea dictumst vestibulum rhoncus est pellentesque. Est placerat in egestas erat imperdiet sed. Lacus viverra vitae congue eu. Sit amet risus nullam eget felis eget nunc. At augue eget arcu dictum varius duis at consectetur. Velit dignissim sodales ut eu. Senectus et netus et malesuada fames ac turpis egestas. Viverra nam libero justo laoreet sit.</p>
-          <form class="flex gap-4 mt-4 items-center flex-wrap">
+          <form class="flex gap-4 mt-4 items-center flex-wrap" @submit.prevent="submitCode">
             <input v-model="offerCode" placeholder="Offer Code" class="rounded h-16 w-44 text-3xl text-center" :disabled="loading"/>
             <pvButton raised v-ripple class="h-16 p-ripple !font-semibold !text-xl" label="Submit" icon="pi pi-arrow-right" iconPos="right" :loading="loading" @click="submitCode" :disabled="!Boolean(offerCode)"/>
             <div v-if="codeError" class="text-xl text-red-700">Invalid Offer Code</div>
@@ -50,7 +50,7 @@ const submitCode = async () => {
   loading.value = true
 
   try {
-
+    console.log('here')
     const { data } = await auth.api.post('/offer-code', { offerCode: offerCode.value })
     templateData.value = data
     console.log(templateData.value)
