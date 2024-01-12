0ab602a72f102bc725f10afa0ed30a4e6701215c
Paul Watson <paulewiii3@gmail.com>
2024-01-11 23:23:05 -0500

    fix mobile menu

--- frontend/src/components/home/NavbarElement.vue
+++ frontend/src/components/home/NavbarElement.vue
@@ -8,13 +8,13 @@
 		<!-- Logo end -->
 
 		<!-- Toggler start -->
-		<button ref="menu" role="menubar" class="md:hidden mr-4" @click="navToggle()">
+		<button role="menubar" class="md:hidden mr-4" @click="navToggle()">
 			<i class="pi pi-align-justify text-white" style="font-size: 2rem"></i>
 		</button>
 		<!-- Toggler end -->
 
 		<!-- Menu content start -->
-		<div class="absolute left-0 right-0 translate-y-16 bg-opacity-90 bg-white md:bg-none md:bg-opacity-0 shadow hidden md:flex flex-col gap-4 items-center p-4 md:flex-row md:static md:shadow-none md:translate-y-0 text-white" role="menu" aria-expanded="false">
+		<div ref="menu" class="absolute left-0 right-0 translate-y-16 bg-opacity-90 bg-primary md:bg-none md:bg-opacity-0 shadow hidden md:flex flex-col gap-4 items-center p-4 md:flex-row md:static md:shadow-none md:translate-y-0 text-white" role="menu" aria-expanded="false">
 			<!-- Links start -->
 			<a href="#" class="menuitem">
 				<p class="mx-4">Home</p>
